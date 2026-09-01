7933b90a05b44eeb946130489b34283e87e28fbe
mtk06230 <utkarsh.tripathi@mediatek.com>
2026-09-01 12:04:28 +0800

    [ALPS11049975] BuildErrorFix: Fix build error
    [Description]
    Patch to fix build error
    - conflicting_type_redefinition: -Wmacro-redefined]
    - conflicting_type_redefinition: -Wmacro-redefined]
    - conflicting_type_redefinition: -Wmacro-redefined]
    - conflicting_type_redefinition: -Wmacro-redefined]
    
    [Test]
    - Verification PASSED
    - Build fail
    
    MTK-Commit-Id: 1076b504f78b9dfa82b6a6f8815e0beb68796d83
    
    CR-Id: ALPS11049975
    Feature: Kernel SI Operation
    Signed-off-by: mtk06230 <utkarsh.tripathi@mediatek.com>
    Change-Id: I7c84ad8565047642b469e24728dd54667f7e26e1

--- connectivity/bt/linux_v2/include/btmtk_define.h
+++ connectivity/bt/linux_v2/include/btmtk_define.h
@@ -121,8 +121,13 @@
 #define IS_NOT_ALIGN_4(_value)      (((_value) & 0x3) ? TRUE : FALSE)
 #endif /* IS_NOT_ALIGN_4 */
 
+#ifndef MIN
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#endif
+
+#ifndef MAX
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
+#endif
 
 
 /**
